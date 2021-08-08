8a088d98b8f75361a425dfde728d80a8f378d899
Eungyeol <eungyeole@naver.com>
2021-08-09 05:09:06 +0900

    style 추가 및 코드 수정

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,37 @@
+const containerElement = document.getElementById("NM_FAVORITE");
 const weatherElement = document.getElementById("NM_WEATHER");
+const url = "https://search.naver.com/search.naver?query=";
+weatherElement.style.height="0px"; weatherElement.style.width="0px";
 
 async function fetcher(){
    const { top10 } = await (await fetch("https://api.signal.bz/news/realtime")).json();
     return top10;
 }
 
-weatherElement.innerHTML="";
 
-fetcher().then((res)=>{
-    const keys = Object.keys(res);
-    const items = keys.map((i)=>RealTimeWordsItem(res[i]));
-    console.log(items);
-    RealItemWordsList(items);
-
-})
+(async ()=>{
+    const data = await fetcher();
+    containerElement.appendChild(RealItemWordsList(data))
+})()
 
 
 function RealItemWordsList(props = []){
-    weatherElement.append(...props)
+    const element = document.createElement("div");
+    const keys = Object.keys(props);
+    element.className="group_keywords";
+    element.append(...keys.map((i)=>RealTimeWordsItem(props[i])))
+    return element;
 }
 
 function RealTimeWordsItem(props){
     const { rank, keyword, state } = props;
-    const url = "https://search.naver.com/search.naver?query=";
-    const item = document.createElement("li");
+    const element = document.createElement("div");
     const aTag = document.createElement("a");
     aTag.href=url+keyword; aTag.className="rank-item";
     aTag.innerHTML = `
         <span class="rank-num">${rank}</span>
         <span class="rank-text">${keyword}</span>
     `
-    item.appendChild(aTag);
-    return item;
+    element.appendChild(aTag);
+    return element;
 }
